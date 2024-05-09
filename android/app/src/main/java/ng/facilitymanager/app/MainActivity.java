edd17b377c27661648fd606950b7aab319011741
Anthony Oyathelemhi <anthony.oyathelemhi@deimos.co.za>
2024-05-09 22:02:15 +0100

    add capacitor

---
+++ android/app/src/main/java/ng/facilitymanager/app/MainActivity.java
@@ -0,0 +1,5 @@
+package ng.facilitymanager.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "ng.facilitymanager.app",
+  appName: "Facility Manager",
+  webDir: "dist",
+};
+
+export default config;
